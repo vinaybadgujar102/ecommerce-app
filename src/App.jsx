3ca7605acbda593e30bbe58ebf5bb558845aca80
vinaybadgujar102 <vinaybadgujar8@gmail.com>
2024-05-25 11:06:49 +0530

    added navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header/Header";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header
+        fixed="top"
+        color="light"
+        light={true}
+        container="md"
+        expand="md"
+      />
+    </>
+  );
 }
 
 export default App;

f34416cafafed43493276c67bb0856b1af237947
vinaybadgujar102 <vinaybadgujar8@gmail.com>
2024-05-25 11:17:45 +0530

    added footer and removed sticky navbar to normal one

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <footer className="footer mt-auto py-3 bg-body-tertiary">
+      <div className="container d-flex justify-content-between">
+        <span className="text-body-secondary">ShopCart© 2023</span>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;

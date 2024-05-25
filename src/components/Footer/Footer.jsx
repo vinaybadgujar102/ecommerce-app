1cb0782af72b79dce02b04f7754d5b2fe3e2d75f
vinaybadgujar102 <vinaybadgujar8@gmail.com>
2024-05-25 11:30:01 +0530

    added homepage ui

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,9 @@
 function Footer() {
   return (
-    <footer className="footer mt-auto py-3 bg-body-tertiary">
+    <footer
+      style={{ marginTop: "5rem" }}
+      className="footer py-3 bg-body-tertiary"
+    >
       <div className="container d-flex justify-content-between">
         <span className="text-body-secondary">ShopCart© 2023</span>
       </div>

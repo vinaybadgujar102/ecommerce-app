e3fafefc7563e049615744f4c9feb4268cf0b3bc
vinaybadgujar102 <vinaybadgujar8@gmail.com>
2024-05-26 12:24:29 +0530

    added product box card to product list page

---
+++ src/components/ProductBox/ProductBox.jsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+import "./ProductBox.css";
+const ProductBox = ({ productImage, name, price }) => {
+  return (
+    <Link
+      to="/products/2"
+      className="product-item text-decoration-none d-inline-block"
+    >
+      <div className="product-img">
+        <img src={productImage} alt="" />
+      </div>
+      <div className="product-name text-center">{name}</div>
+      <div className="product-price text-center">&#8377; {price}</div>
+    </Link>
+  );
+};
+
+export default ProductBox;
--- src/pages/ProductList/ProductList.jsx
+++ src/pages/ProductList/ProductList.jsx
@@ -1,5 +1,24 @@
+import ProductBox from "../../components/ProductBox/ProductBox";
+import "./ProductList.css";
+
 const ProductList = () => {
-  return <div>ProductList</div>;
+  return (
+    <div className="container">
+      <div className="row">
+        <h2 className="product-list-title text-center">All Products</h2>
+        <div className="product-list-wrapper d-flex flex-column">
+          {/* List of prodcuts /> */}
+          <div className="product-list-box" id="productList">
+            <ProductBox
+              productImage="https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+              name="Product Name"
+              price="100"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProductList;

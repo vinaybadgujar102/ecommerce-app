73fb3fdd7a77724b7c1fbb1af862526b72fdfcbd
vinaybadgujar102 <vinaybadgujar8@gmail.com>
2024-05-26 14:32:04 +0530

    added product details page

--- src/routes/MainRoutes.jsx
+++ src/routes/MainRoutes.jsx
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router-dom";
 import { Home } from "../pages/Home/Home";
 import Error from "../pages/Error/Error";
 import ProductList from "../pages/ProductList/ProductList";
+import ProductDetails from "../pages/ProductDetails/ProductDetails";
 
 function MainRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/products" element={<ProductList />} />
+      <Route path="/products/:id" element={<ProductDetails />} />
       <Route path="*" element={<Error />} />
 
       <Route />
